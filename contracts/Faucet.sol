92b6892e4925b40d37929a82dff766bf5fef55d3
Carles Saez Marti <csm132@alu.ua.es>
2021-06-26 16:09:11 +0200

    arreglada faucet

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract IERC20 {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
 
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -18,7 +18,7 @@ contract Faucet {
   mapping(IERC20=>address) owner;
   mapping(IERC20=>uint16) secs;
 
-  constructor() {
+  constructor() public {
     adminFaucet = msg.sender;
   }
 
@@ -26,7 +26,7 @@ contract Faucet {
     require(msg.sender == adminFaucet);
     _;
   }
-  modifier onlyOwner(){
+  modifier onlyOwner(IERC20 token){
     require(owner[token] == msg.sender);
     _;
   }
@@ -34,21 +34,21 @@ contract Faucet {
   function receiveTokens(uint amount, IERC20 token) external{
     require(owner[token] == address(0));
     owner[token] = msg.sender;
+    token.transfer(address(this), amount);
   }
 
   function claimed(IERC20 token, uint amount) external{
-    uint256 amount = amount;
-    require(expiryOf[token][msg.sender] < block.timestamp + secs);
+    require(expiryOf[token][msg.sender] < block.timestamp + secs[token]);
     token.transfer(msg.sender, amount);
-    expiryOf[token][msg.sender] = block.timestamp + secs;
+    expiryOf[token][msg.sender] = block.timestamp + secs[token];
   }
 
   function setSecs(uint16 _secs, IERC20 token) external onlyAdmin{
     secs[token] = _secs;
   }
 
-  function vaciarFaucet(IERC20 token) external onlyOwner{
-    token.transfer(token.balanceOf(address(this), msg.sender));
+  function vaciarFaucet(IERC20 token) external onlyOwner(token){
+    token.transfer(msg.sender, token.balanceOf(address(this)));
   }
 
 
