6751f9626dcd772ab1040c2b150f81ced4191584
Guillermo Rivero <guillermo.rivero@ofertia.com>
2021-06-26 19:12:26 +0200

    Initial commit

--- contracts/token/TuviellaToken.sol
+++ contracts/token/TuviellaToken.sol
@@ -2,6 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TuviellaToken {
+
+  string  public name = "Tuviella Token";
+  string  public symbol = "VIELLA";
+  uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+  uint8   public decimals = 18;
+
   constructor() public {
+    
   }
 }
