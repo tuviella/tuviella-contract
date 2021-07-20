acec5b9ec38c3cbf7c98a95b2dfcb36de3379b83
Carles Saez Marti <csm132@alu.ua.es>
2021-07-20 17:46:23 +0200

    faucet funcional

--- contracts/token/UwUtoken.sol
+++ contracts/token/UwUtoken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract randomToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("randomToken", "rand") {
+contract UwUToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("UwUtoken", "UwU") {
         _mint(msg.sender, initialSupply);
     }
 }
