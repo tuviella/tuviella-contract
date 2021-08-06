ebc4cd7316f683a81e27c5e1569eef8f70db8b84
Carles Saez Marti <csm132@alu.ua.es>
2021-08-06 21:38:46 +0200

    Arregladas carpetas del proyecto

--- contracts/token/TuviellaToken.sol
+++ contracts/token/TuviellaToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-import "../contracts/TransactionFee.sol";
+import "../library/TransactionFee.sol";
 
 contract TuviellaToken is ERC20, ERC20Burnable, ERC20Pausable, AccessControlEnumerable, TransactionFee {
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
